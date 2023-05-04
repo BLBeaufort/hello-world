f1322c3a913bd4c828da996ff3782faa5c8cce20
BLBeaufort <wontcomprms@yahoo.com>
2023-05-03 20:11:10 -0400

    oc06.sh

--- oc06.sh
+++ oc06.sh
@@ -3,17 +3,14 @@
 # Date of latest revision: 5/3/2023
 # Purpose:  Create a loop/conditional statements
 
-#! /bin/bash
-
-directories-("dir5 "dir6" "dir7")
-
-for directory in "$[directories[@]}"; do
-
-if [!-b "$directories"]; then
+#!/bin/bash
+directories=("dir1" "dir2" "dir3")
+for directory in "${directories[@]}"; do
+#Start if statement, ! -d is checking the condition to see if directory exzists.  If it does not, it will create a directory.
+if [!-f "$directories"]; then
 mkdir "$directory"
-
+#If directory does not exist, print to terminal "directory already exists"
 else
 echo "directory exists"
 fi
 done
-
