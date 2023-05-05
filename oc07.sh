ac7c6d274d8c95bfd91e50f1d8da8b7d5abf9302
BLBeaufort <wontcomprms@yahoo.com>
2023-05-04 20:08:57 -0400

    oc07.sh

--- oc07.sh
+++ oc07.sh
@@ -5,47 +5,46 @@
 
 #!/bin/bash
 
-#!bin/bash/
-
-echo “Computer Name”
+echo “System Information”
 
 echo “CPU”
-lshw -c cpu | grep -i Product
-lshw -c cpu | grep -i Vendor
-lshw -c cpu | grep -i Physical ID
-lshw -c cpu | grep -i Bus information
-lshw -c cpu | grep -i Width
+ lshw -c cpu | grep -i “Product”
+ lshw -c cpu | grep -i “Vendor”
+ lshw -c cpu | grep -i “Physical ID”
+ lshw -c cpu | grep -i “Bus information”
+ lshw -c cpu | grep -i “Width”
 
 echo “RAM”
-lshw -c ram | grep -i Description
-lshw -c ram | grep -i Physical ID
-lshw -c ram | grep -i Size
+ lshw -c memory | grep -i “Description”
+ lshw -c memory | grep -i “Physical ID”
+ lshw -c memory | grep -i “Size”
 
 echo “Display Adapter”
-lshw -c display| grep -i Description
-lshw -c display | grep -i Product
-lshw -c display| grep -i Vendor
-lshw -c display | grep -i Physical ID
-lshw -c display | grep -i Bus info
-lshw -c display | grep -i Width
-lshw -c display | grep -i Clock
-lshw -c display | grep -i Capabilities
-lshw -c display | grep -i Configuration
-lshw -c display | grep -i Resources
+ lshw -c display | grep -i “Description”
+ lshw -c display | grep -i “Product”
+ lshw -c display | grep -i “Vendor”
+ lshw -c display | grep -i “Physical ID”
+ lshw -c display | grep -i “Bus Info”
+ lshw -c display | grep -i “Width”
+ lshw -c display | grep -i “Clock”
+ lshw -c display | grep -i “Capabilities”
+ lshw -c display | grep -i “Configuration”
+ lshw -c display | grep -i “Resources”
 
 echo “Network Adapter”
-lshw -c display| grep -i Description
-lshw -c display | grep -i Product
-lshw -c display| grep -i Vendor
-lshw -c display | grep -i Physical ID
-lshw -c display | grep -i Bus info
-lshw -c display | grep -i Logical name
-lshw -c display | grep -i Version
-lshw -c display | grep -i Serial
-lshw -c display | grep -i Size
-lshw -c display | grep -i Capacity
-lshw -c display | grep -i Width
-lshw -c display | grep -i Clock
-lshw -c display | grep -i Capabilities
-lshw -c display | grep -i Configuration
-lshw -c display | grep -i Resources
+ lshw -c network | grep -i “Description”
+ lshw -c network | grep -i “Product”
+ lshw -c network | grep -i “Vendor”
+ lshw -c network | grep -i “Physical ID”
+ lshw -c network | grep -i "Bus Info"
+ lshw -c network | grep -i "Logical Name"
+ lshw -c network | grep -i "Version"
+ lshw -c network | grep -i "Serial"
+ lshw -c network | grep -i "Size"
+ lshw -c network | grep -i "Capacity"
+ lshw -c network | grep -i "Width"
+ lshw -c network | grep -i "Clock"
+ lshw -c network | grep -i "Capabilities"
+ lshw -c network | grep -i "Configuration"
+ lshw -c network | grep -i "Resources"
+ 
