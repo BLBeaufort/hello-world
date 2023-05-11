8ff9439fb95bf56a352445d035b801ea2f80ac3e
BLBeaufort <wontcomprms@yahoo.com>
2023-05-11 18:05:51 -0400

    opsc10.sh

--- opsc10.sh
+++ opsc10.sh
@@ -6,36 +6,36 @@
 #Task 1
 #Print to the terminal screen all active processes ordered by highest CPU time consumption at the top.
 
-Get-Process | Sort-Object CPU -Descending
+$Get-Process | Sort-Object CPU -Descending
 
 #Task 2
 #Print to the terminal screen all active processes ordered by highest Process Identification Number at the top.
 
-Get-Process | Sort-Object ID -Descending
+$Get-Process | Sort-Object ID -Descending
 
 #Task 3
 #Print to the terminal screen the top five active processes ordered by highest Working Set (WS(k)) at the top.
 
-Get-Process | Sort-Object WS -Descending | Select -First 5
+$Get-Process | Sort-Object WS -Descending | Select -First 5
 
 #Task 4
 #Start a browser process (such as Google Chrome or MS Edge) and have it open https://owasp.org/www-project-top-ten/.
 
-Start-Process "https://owasp.org/www-project-top-ten/"
+$Start-Process "https://owasp.org/www-project-top-ten/"
 
 #Task 5
 #Start the process Notepad ten times using a for loop.
 
-for ($i=1; $i -le 10; $i++)
-{Start-Process 'C:\Windows\system32\notepad.exe'}
+$for ($i=1; $i -le 10; $i++)
+${Start-Process 'C:\Windows\system32\notepad.exe'}
 
 #Task 6
 #Close all instances of the Notepad.
 
-Stop-Process -Name "notepad"
+$Stop-Process -Name "notepad"
 
 #Task 7
 #Kill a process by its Process Identification Number.  Choose a process whose termination won't destabilize the system, such as Google Chrome or MS Edge.
 #MSEdge PID for this instance is 1576.
 
-Taskkill /PID 1576 /F
+$Taskkill /PID 1576 /F
