b1be678301ca2a8563fa7d8feee294a92e28536a
BLBeaufort <BLBeaufort@gmail.com>
2023-05-31 17:08:12 -0400

    opsc02.sh

---
+++ opsc02.sh
@@ -0,0 +1,10 @@
+#!/bin/sh.
+#Script Name-opsc02.sh
+#Author-Barbara L. Beaufort
+#Date of Last Revision-31 May 2023
+#Description of Purpose-Copy system log files to the PWD and appends current date and time.
+#Main
+
+cp -v /var/log/syslog "$PWD/syslog-$(date +"%Y-%m-%d-%H:%M:%S")"
+
+#End
