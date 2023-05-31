a7607c819ee5101ff23be613b93b402ca126460f
BLBeaufort <BLBeaufort@gmail.com>
2023-05-31 17:26:57 -0400

    ops301-2.sh

--- ops301-2.sh
+++ ops301-2.sh
@@ -1,8 +1,8 @@
-#!/bin/sh.
-#Script Name-ops301-2.sh
-#Author-Barbara L. Beaufort
-#Date of Last Revision-31 May 2023
-#Description of Purpose-Copy system log files to the PWD and appends current date and time.
+#!/bin/sh
+#Script Name: ops301-2.sh
+#Author: Barbara L. Beaufort
+#Date of Last Revision: 31 May 2023
+#Description of Purpose: Copy system log files to the PWD and appends current date and time.
 #Main
 
 cp -v /var/log/syslog "$PWD/syslog-$(date +"%Y-%m-%d-%H:%M:%S")"
