da46bc4486eb0af7807f1031c539ff5e565e1010
BLBeaufort <BLBeaufort@gmail.com>
2023-06-01 20:22:11 -0400

    ops301-2.sh

--- ops301-2.sh
+++ ops301-2.sh
@@ -15,7 +15,7 @@ filename=syslog_$now
 
 
 echo "Creating system log in working directory"
-cp /var/log/syslog ~/Desktop/Journal 08.docx
+cp /var/log/syslog ~/Desktop/Journal 08.doc/
 
 echo "Displaying timestamp in filename below"
 echo $filename
