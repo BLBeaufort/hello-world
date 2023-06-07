aa994c4366c9d0c54092be7ec3dfd59c3062239c
BLBeaufort <BLBeaufort@gmail.com>
2023-06-07 19:56:40 -0400

    opsc301-3.sh

--- opsc301-3.sh
+++ opsc301-3.sh
@@ -4,23 +4,12 @@
 #Date of Last Revision: 07 June 2023
 #Description of Purpose: Changing permissions in system files/directories
 
-#Get a list of files in directory
-files=$(ls ${dir})
-
-#Prompt user for input of directory path
-read -p "Please Enter Path:" -r r1
-parent=$(dirname "$r1")
-cd "$parent
-
-#Prompt user for input of permissions number
-    echo "Enter name of File"
+#Main
+    echo "Enter a File"
     ls
     read file
     echo "Enter Permissions"
     read number
-    $777="chmod777"
-    read my_var
-    ls -l $file 
-    echo "Welcome User"
+    chmod $num $file
     ls -l $file
     
