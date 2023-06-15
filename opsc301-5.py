93e18338fd970ecdef768f12b7d80e3345017554
BLBeaufort <BLBeaufort@gmail.com>
2023-06-14 20:07:10 -0400

    opsc301-5.py

---
+++ opsc301-5.py
@@ -0,0 +1,20 @@
+# Write a program that works out whether if a given number is an 
+# odd or even number
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+num = int(input(“Which number do you want to check? ”))
+
+if (num % 2) == 0:
+    
+    print (“The number is even”)
+    
+else:
+    print ("The number is odd")
+    
+Output:
+    
+Enter any number to test whether it is odd or even:
