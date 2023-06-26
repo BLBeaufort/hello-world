726ed8b9cd6694e8c3bb2ef6cbaf18aa51121eff
BLBeaufort <BLBeaufort@gmail.com>
2023-06-26 18:37:59 -0400

    opsc301-8.py

---
+++ opsc301-8.py
@@ -0,0 +1,22 @@
+#script: Ops Challenge: Class 08
+# Author:  Barbara L. Beaufort
+# Date of latest revision: 6/26/2023
+# Purpose:  Strings and Elements
+#Main
+
+print ("List of Words Dealing With Summertime Cooking")
+
+string = "Food Words"
+a = "vinegar"; b = "crushed red peppers"; c = "hot water"; d = "poultry"; e = "bbq sauce"; f = "meat mop"; g = "garlic bulbs"; h = "olive oil"; i = "eggs"; j = "grill"
+print (d)
+print (f, g, h, i, j)
+k_list = ['garlic bulbs']
+
+for f in range(len(k_list)):
+    if k_list[f] == 'garlic bulbs':
+        k_list[f] = 'onion'
+
+print(k_list)
+
+
+
