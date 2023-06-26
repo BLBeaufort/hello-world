0441dacadf0c71a871cac6c62b75f46ef7d40717
BLBeaufort <BLBeaufort@gmail.com>
2023-06-26 19:58:00 -0400

    opsc301-8.py

--- opsc301-8.py
+++ opsc301-8.py
@@ -10,11 +10,11 @@ string = "Food Words"
 a = "vinegar"; b = "crushed red peppers"; c = "hot water"; d = "poultry"; e = "bbq sauce"; f = "meat mop"; g = "garlic bulbs"; h = "olive oil"; i = "eggs"; j = "potato salad"
 print (d)
 print (f, g, h, i, j)
-k_list = ['garlic bulbs']
+k_list = ["garlic bulbs"]
 
 for f in range(len(k_list)):
-    if k_list[f] == 'garlic bulbs':
-        k_list[f] = 'onion'
+    if k_list[f] == "garlic bulbs":
+        k_list[f] = "onion"
 
 print(k_list)
 
