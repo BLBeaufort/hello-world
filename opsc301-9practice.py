44d3b815cd177725d9e08b4dd5f33fabbca5d263
BLBeaufort <BLBeaufort@gmail.com>
2023-06-28 19:37:39 -0400

    opsc301-9.py

---
+++ opsc301-9practice.py
@@ -0,0 +1,33 @@
+# script: Ops Challenge: Class 09
+# Author:  Barbara L. Beaufort
+# Date of latest revision: 6/27/2023
+# Purpose:  Python Conditional Statements
+#Main
+
+#Equals Condition
+x = 1964
+y = 2023
+if y > x:
+    print ("y is greater than x")
+
+#Not Equals Condition
+a = 33
+b = 250
+if a != b:
+    print ("a does not equal b") # you will get an error
+    
+#Less Than Condition
+d = 99
+e = 135
+if d < e:
+    print ("d is less than e") 
+
+#Less Than or Equal to Condition
+f = 348
+g = 225
+if g <= f:
+    print ("Life is Awesome!")
+    
+    
+    
+    
